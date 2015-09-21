e7406a92af8d35ea90aed5c768bfd8868be8951c
mellanon <andreas.aastroem@gmail.com>
2015-09-22 00:01:55 +1200

    Optimised speedtest_cli.py for upload speeds

---
+++ SpeedDevice/sendQueue.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import pika
+import yaml
+
+with open('/opt/SpeedDevice/config.yaml', 'r') as f:
+    config = yaml.load(f)
+
+##Rabbit MQ configuration
+username = config["rabbit-mq"]["username"]
+password = config["rabbit-mq"]["password"]
+sendQueue = config["rabbit-mq"]["send-queue"]
+
+credentials = pika.PlainCredentials(username, password)
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+               'localhost', 5672, '/', credentials))
+channel = connection.channel()
+channel.queue_declare(queue=sendQueue)
+
+print ' [*] Waiting for messages. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
+
+channel.basic_consume(callback,
+                      queue=sendQueue,
+                      no_ack=True)
+
+channel.start_consuming()
